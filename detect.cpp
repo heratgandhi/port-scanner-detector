df397cb386f652d448fb66b29a951d778e5e9deb
Herat <hag59@cornell.edu>
2013-10-03 19:58:45 -0400

    Added useful code

--- detect.cpp
+++ detect.cpp
@@ -1,72 +1,90 @@
 #include <pcap.h>
 #include <stdio.h>
-#include <stdlib.h>         //for exit()
-#include <string.h>         //for memset
- 
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>      //for inet_ntoa()
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
 #include <net/ethernet.h>
-#include <netinet/tcp.h>    //Provides declarations for tcp header
-#include <netinet/ip.h>     //Provides declarations for ip header
-
+#include <netinet/ip.h>
 #include <map>
 #include <utility>
 #include <iostream>
-
-#define THRESHOLD 100
+#include <time.h>
 
 using namespace std;
 
+struct stat
+{
+	int state;
+};
+
+struct storage
+{
+	int full_open;
+	int half_open;
+	int reset;
+	int data_transfer;
+	map<int,stat> port;
+};
+
 void process_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
-void process_ip_packet(const u_char * , int);
 void print_tcp_packet(const u_char *  , int );
-void PrintData (const u_char * , int);
- 
-FILE *logfile;
+
 struct sockaddr_in source,dest;
 int tcp=0,others=0,total=0,i,j;
-map<string, int> logger;
 string key;
-
-int opp,mine;
+int key1;
+stat temp;
 int syn,rst,ack,fin;
- 
-int main()
-{
-    pcap_if_t *alldevsp , *device;
-    pcap_t *handle; //Handle of the device that shall be sniffed
- 
-    char errbuf[100] , *devname;
-    int count = 1 , n;
-     
-    devname = "eth0";
-     
-    //Open the device for sniffing
-    //printf("Opening device %s for sniffing ... " , devname);
-    //handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
-    handle = pcap_open_offline("/home/herat/Desktop/data.pcap",errbuf);
-     
-    if (handle == NULL) 
-    {
-        fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
-        exit(1);
-    }
-    printf("Done\n");
-     
-    logfile=fopen("log.txt","w");
-    if(logfile==NULL) 
-    {
-        printf("Unable to create file.");
-    }
-     
-    //Put the device in sniff loop
-    pcap_loop(handle , -1 , process_packet , NULL);
 
-	for( map<string, int>::iterator ii=logger.begin(); ii!=logger.end(); ++ii)
+string myip = "192.168.48.146";
+
+map<string, storage> logger;
+ 
+int main(int argc,char*argv[])
+{
+    pcap_t *handle;
+    int mode;
+    char errbuf[100] , *devname;
+    char *fname;
+    if(argc < 4)
+    {
+    	cout<<"Usage: ./Program Mode Options Your_IP\n";
+    	cout<<"        Mode: 1 - File, 2 - Device\n";
+    	cout<<"        Options: 1 - File name\n";
+    	cout<<"        Options: 2 - Interface name\n";
+    	exit(1);
+    }
+    mode = atoi(argv[1]);
+    if(mode == 1)
+    {
+    	myip = argv[3];
+    	fname = argv[2];
+    	handle = pcap_open_offline(fname,errbuf);
+    	pcap_loop(handle ,-1, process_packet , NULL);
+    }
+    else if(mode == 2)
+    {
+    	myip = argv[3];
+    	devname = argv[2];
+    	handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
+    	if (handle == NULL)
+		{
+			fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
+			exit(1);
+		}
+    	pcap_loop(handle, 1000, process_packet, NULL);
+    }
+
+	for(map<string,storage>::iterator ii=logger.begin(); ii!=logger.end(); ++ii)
 	{
-		if((*ii).second > THRESHOLD)
-			cout << (*ii).first << ": " << (*ii).second << endl;
+		key = (*ii).first;
+		cout<<key<<endl;
+		cout<<(*ii).second.data_transfer<<" "<<(*ii).second.full_open<<" "<<
+				(*ii).second.half_open<<" "<<(*ii).second.reset<<endl;
 	}
+
     return 0;
 }
  
@@ -93,6 +111,7 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 void print_tcp_packet(const u_char * Buffer, int Size)
 {
     unsigned short iphdrlen;
+    int destp,srcp;
     struct iphdr *iph = (struct iphdr *)( Buffer  + sizeof(struct ethhdr) );
     iphdrlen = iph->ihl*4;
     struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen + sizeof(struct ethhdr));
@@ -105,7 +124,93 @@ void print_tcp_packet(const u_char * Buffer, int Size)
 	memset(&dest, 0, sizeof(dest));
 	dest.sin_addr.s_addr = iph->daddr;
 
-	/*fprintf(logfile , "\n\n***********************TCP Packet*************************\n");
+	syn = (unsigned int)tcph->syn;
+	rst = (unsigned int)tcph->rst;
+	fin = (unsigned int)tcph->fin;
+	ack = (unsigned int)tcph->ack;
+
+	destp = ntohs(tcph->dest);
+	srcp = ntohs(tcph->source);
+
+	//cout<<inet_ntoa(source.sin_addr) << " ";
+	//cout<<inet_ntoa(dest.sin_addr)<<endl;
+	//cout <<syn<<" "<<rst<<" "<<fin<<" "<<endl;
+	//cout << destp << " "<< srcp << "\n";
+
+	//Handle incoming SYN packets
+	if(strcmp(inet_ntoa(source.sin_addr),myip.c_str()) != 0 && syn == 1 && ack == 0)
+    {
+		key = inet_ntoa(source.sin_addr);
+		key1 = destp;
+		temp.state = 1;
+		logger[key].port[key1] = temp;
+    }
+	//Handle outgoing SYN+ACK packets
+    else if(strcmp(inet_ntoa(source.sin_addr),myip.c_str()) == 0 && syn == 1 && ack == 1)
+    {
+    	key = inet_ntoa(dest.sin_addr);
+    	key1 = srcp;
+    	if(logger[key].port[key1].state == 1)
+    	{
+			temp.state = 2;
+			logger[key].port[key1] = temp;
+    	}
+    }
+	//Handle incoming ACK packets
+    else if(strcmp(inet_ntoa(source.sin_addr),myip.c_str()) != 0 && ack == 1 && tcph->seq == 1)
+	{
+    	key = inet_ntoa(source.sin_addr);
+		key1 = destp;
+		if(logger[key].port[key1].state == 2)
+		{
+			temp.state = 3;
+			logger[key].port[key1] = temp;
+			logger[key].full_open++;
+		}
+	}
+	//Handle incoming RST packets
+    else if(strcmp(inet_ntoa(source.sin_addr),myip.c_str()) != 0 && rst == 1)
+	{
+    	//cout<<"incoming rst\n";
+    	key = inet_ntoa(source.sin_addr);
+		key1 = destp;
+		if(logger[key].port[key1].state == 2)
+		{
+			temp.state = 4;
+			logger[key].port[key1] = temp;
+			logger[key].half_open++;
+			//cout<<"Increment\n";
+		}
+	}
+	//Handle outgoing RST packets
+    else if(strcmp(inet_ntoa(source.sin_addr),myip.c_str()) == 0 && rst == 1)
+	{
+    	//cout<<"outgoing rst\n";
+    	key = inet_ntoa(dest.sin_addr);
+		key1 = srcp;
+		if(logger[key].port[key1].state == 1)
+		{
+			temp.state = 3;
+			logger[key].port[key1] = temp;
+			logger[key].reset++;
+			//cout<<key<<" Increment \n";
+			//cout<<key1<<" Increment \n";
+		}
+	}
+	//Handle data transfer
+    else if(strcmp(inet_ntoa(source.sin_addr),myip.c_str()) != 0  && ack == 1 && tcph->seq > 1)
+    {
+    	key = inet_ntoa(source.sin_addr);
+		key1 = destp;
+		if(logger[key].port[key1].state == 3)
+		{
+			temp.state = 4;
+			logger[key].port[key1] = temp;
+			logger[key].data_transfer++;
+		}
+    }
+}
+/*fprintf(logfile , "\n\n***********************TCP Packet*************************\n");
 	fprintf(logfile , "\n");
 	fprintf(logfile , "Ethernet Header\n");
 	fprintf(logfile , "   |-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5] );
@@ -149,38 +254,3 @@ void print_tcp_packet(const u_char * Buffer, int Size)
     fprintf(logfile , "\n");
 
     fprintf(logfile , "\n###########################################################");*/
-
-    syn = (unsigned int)tcph->syn;
-    rst = (unsigned int)tcph->rst;
-    fin = (unsigned int)tcph->fin;
-    ack = (unsigned int)tcph->ack;
-
-    if(syn == 1 && ack == 0)
-    {
-    	key = inet_ntoa(source.sin_addr);
-    	if(logger.find(key) == logger.end())
-		{
-			//not found key
-			logger[key] = 1;
-		}
-		else
-		{
-			//found key
-			logger[key] += 1;
-		}
-    }
-    if(rst == 1)
-    {
-    	key = inet_ntoa(dest.sin_addr);
-		if(logger.find(key) == logger.end())
-		{
-			//not found key
-			logger[key] = 1;
-		}
-		else
-		{
-			//found key
-			logger[key] += 1;
-		}
-    }
-}
